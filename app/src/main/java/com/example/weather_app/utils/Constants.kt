e0db10b4caa3f6a02ea1fefd26bd2bc6c2bc3be9
HARSHPARMAR1281 <parmarharsh1281@gmail.com>
2025-05-22 18:54:55 +0530

    Update Constants.kt

--- app/src/main/java/com/example/weather_app/utils/Constants.kt
+++ app/src/main/java/com/example/weather_app/utils/Constants.kt
@@ -18,24 +18,21 @@ object Constants {
     const val EXTRA_LATITUDE = "latitude"
     const val EXTRA_LONGITUDE = "longitude"
     
-    // Animation Durations
-    const val SPLASH_DELAY = 2000L // 2 seconds
     
-    // Weather Units
-    const val TEMP_UNIT = "metric" // Celsius
-    const val WIND_UNIT = "metric" // m/s
+    const val SPLASH_DELAY = 2000L 
     
-    // Location
+    const val TEMP_UNIT = "metric" 
+    const val WIND_UNIT = "metric" 
+    
     const val LOCATION_PERMISSION_REQUEST_CODE = 1001
-    const val DEFAULT_LOCATION_UPDATE_INTERVAL = 10000L // 10 seconds
-    const val DEFAULT_LOCATION_FASTEST_INTERVAL = 5000L // 5 seconds
+    const val DEFAULT_LOCATION_UPDATE_INTERVAL = 10000L 
+    const val DEFAULT_LOCATION_FASTEST_INTERVAL = 5000L 
     
-    // MongoDB Configuration
+    
     const val MONGODB_USERNAME = "parmarharsh1281"
     const val MONGODB_PASSWORD = "hloindia@123"
     const val MONGODB_CLUSTER = "cluster0.hxgdujd"
     const val MONGODB_DATABASE = "weather_data"
-    
-    // Construct MongoDB connection string
+
     val MONGODB_CONNECTION_STRING = "mongodb+srv://$MONGODB_USERNAME:$MONGODB_PASSWORD@$MONGODB_CLUSTER.mongodb.net/$MONGODB_DATABASE?retryWrites=true&w=majority&appName=Cluster0"
 } 
